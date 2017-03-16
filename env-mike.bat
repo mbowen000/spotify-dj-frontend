f56e7d38e6678169a03152cef13dfa81f35563a5
MikeBowen <mike@smbhd.com>
2017-03-15 19:02:45 -0500

    adding test endpoint

---
+++ env-mike.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET client_id=5cc6f71703df4006b7d77b7e2dd85d97
+SET client_secret=aec64438e1b742558a6d2483d060b74d
+SET MONGOLAB_URI=mongodb://teamtunes:a1234567!@ds043615.mlab.com:43615/heroku_wtv92whl
+SET play_passphrase=ilovejulia
+SET redirect_uri=http://localhost:3000/callback
