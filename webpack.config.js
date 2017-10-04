b7fd2f2414371f6e0eb61fe6c0c1b0360f996310
MJ <mike.scott.bowen@gmail.com>
2017-10-04 14:53:27 -0500

    new build with proper node_env for production...

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
-console.log(path.join(__dirname, 'config', 'development'));
+console.log(path.join(__dirname, 'src/config', process.env.NODE_ENV));
 
 module.exports = {
   entry: './src/app.js',
